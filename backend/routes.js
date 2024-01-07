86c778661174fe05f9b26c040ac538456587546e
Patrick Palmroos <palmroos.patrick@gmail.com>
2024-01-08 00:57:18 +0200

    Add: added routes.js for fetch call routes, has one for getting all tasks now

---
+++ backend/routes.js
@@ -0,0 +1,7 @@
+const db = require("../database/tasksDB");
+const express = require("express");
+const router = express.Router();
+
+router.get("/tasks", async (req, res) => {
+  await db.getAllTasks().then((dbItems) => res.json(dbItems));
+});
