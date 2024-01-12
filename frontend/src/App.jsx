642398664b1e72edc2825b3227ece397c29fc03e
Patrick Palmroos <palmroos.patrick@gmail.com>
2024-01-13 00:57:41 +0200

    change: removed axios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
-import { useState, useEffect } from "react";
 import "./App.css";
-import axios from "axios";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar.jsx";
@@ -8,16 +6,8 @@ import Home from "./pages/Home.jsx";
 import Play from "./pages/Play.jsx";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-
-  axios
-    .get(`${import.meta.env.VITE_API_URL}/tasks`)
-    .then((resp) => setTasks(resp.data[0].TaskID))
-    .catch((err) => console.log(err));
-
   return (
     <>
-      <h1>{tasks}</h1>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Navbar />}>
