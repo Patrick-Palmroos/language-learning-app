770fce46e4ea1b24ca30b6f1bc54129326003fc5
Patrick Palmroos <palmroos.patrick@gmail.com>
2024-01-13 01:21:43 +0200

    Fix: fixed tasks.map running on an empty array and causing an issue

--- frontend/src/pages/Play.jsx
+++ frontend/src/pages/Play.jsx
@@ -2,26 +2,29 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 function Play() {
-  const [tasks, setTasks] = useState([{}]);
-
+  const [tasks, setTasks] = useState([]);
   useEffect(() => {
     axios
       .get(`${import.meta.env.VITE_API_URL}/tasks`)
       .then((resp) => setTasks(resp.data))
       .catch((err) => console.log(err));
   }, []);
-
   return (
     <div>
-      <ul>
-        {tasks.map((task) => {
-          return (
-            <li
-              key={task.TaskID}
-            >{`task in english: ${task.English}, finnish: ${task.Finnish}, swedish: ${task.Swedish}`}</li>
-          );
-        })}
-      </ul>
+      {tasks.length > 0 ? (
+        <ul>
+          {tasks.map((task) => {
+            console.log(task);
+            return (
+              <li
+                key={task.TaskID}
+              >{`task in english: ${task.English}, finnish: ${task.Finnish}, swedish: ${task.Swedish}`}</li>
+            );
+          })}
+        </ul>
+      ) : (
+        <p>Loading data</p>
+      )}
     </div>
   );
 }
