a86e50e01621e972782e2a3b30010182ef394107
Patrick Palmroos <palmroos.patrick@gmail.com>
2024-01-13 00:20:49 +0200

    change: App.jsx now displays demo data from database and updated .json files dependencies for react-router-dom. Also App.jsx has routes setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,31 @@
 import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import axios from "axios";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar.jsx";
+import Home from "./pages/Home.jsx";
+import Play from "./pages/Play.jsx";
 
 function App() {
   const [tasks, setTasks] = useState([]);
 
   axios
     .get(`${import.meta.env.VITE_API_URL}/tasks`)
-    .then((resp) => console.log(resp.data))
+    .then((resp) => setTasks(resp.data[0].TaskID))
     .catch((err) => console.log(err));
 
   return (
     <>
       <h1>{tasks}</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navbar />}>
+            <Route index element={<Home />} />
+            <Route path="play" element={<Play />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
