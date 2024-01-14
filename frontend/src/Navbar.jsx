3b903c7adf97b944b90e7bbc496fd162a5de3a59
Patrick Palmroos <palmroos.patrick@gmail.com>
2024-01-14 02:39:13 +0200

    change: made profile into a dropdown menu

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,6 +1,13 @@
 import { Outlet, Link } from "react-router-dom";
+import { useState } from "React";
 
 function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  const handleDropdown = () => {
+    setOpen(!open);
+  };
+
   return (
     <>
       <nav id="navbar">
@@ -12,10 +19,16 @@ function Navbar() {
             <Link to="play">Play</Link>
           </li>
         </ul>
-        <ul>
-          <li>settings</li>
-          <li>Login</li>
-        </ul>
+        <div className="dropDown">
+          <button onClick={handleDropdown}>Profile</button>
+          {open ? (
+            <ul className="menu-items">
+              <li>profile</li>
+              <li>settings</li>
+              <li>login</li>
+            </ul>
+          ) : null}
+        </div>
       </nav>
 
       <Outlet />
