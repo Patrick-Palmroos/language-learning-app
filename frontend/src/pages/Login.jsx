42b8243a6c10c38d3e2d874e1b7e2dbcbf99e381
Patrick Palmroos <palmroos.patrick@gmail.com>
2024-01-15 01:54:42 +0200

    change: textfield inputs now stored in a useState

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,9 @@
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useState } from "react";
 
+//theme for handling the textfield colors. Figuring this out caused too much headache..
 const theme = createTheme({
   components: {
     MuiInputLabel: {
@@ -15,10 +17,10 @@ const theme = createTheme({
       styleOverrides: {
         root: {
           "&:hover .MuiOutlinedInput-notchedOutline": {
-            borderColor: "white", // Outline color
+            borderColor: "white",
           },
           "& .MuiOutlinedInput-notchedOutline": {
-            borderColor: "white", // Outline color
+            borderColor: "white",
           },
         },
       },
@@ -27,7 +29,27 @@ const theme = createTheme({
 });
 
 function Login() {
+  const [usernameInput, setUsernameInput] = useState("");
+  const [passwordInput, setPasswordInput] = useState("");
+
+  //handles storing typing for password field
+  const handlePasswordInput = (event) => {
+    setPasswordInput(event.target.value);
+    console.log(passwordInput);
+  };
+
+  //handles storing typing for username field
+  const handleUsernameInput = (event) => {
+    setUsernameInput(event.target.value);
+    console.log(usernameInput);
+  };
+
+  const handleLogin = () => {
+    console.log(`Username: ${usernameInput} \nPassword: ${passwordInput}`);
+  };
+
   return (
+    //theme provider which hauses input fields.
     <ThemeProvider theme={theme}>
       <Box
         component="form"
@@ -43,12 +65,14 @@ function Login() {
             id="outlined-required"
             label="Username"
             InputProps={{ style: { color: "white" } }}
+            onChange={handleUsernameInput}
           />
           <TextField
             id="outlined-password-input"
             label="Password"
             type="password"
             InputProps={{ style: { color: "white" } }}
+            onChange={handlePasswordInput}
           />
         </div>
       </Box>
