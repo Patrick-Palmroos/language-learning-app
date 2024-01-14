46251e5483e51c22f1d38d6aac0317c9c4b3f959
Patrick Palmroos <palmroos.patrick@gmail.com>
2024-01-14 04:39:03 +0200

    change: login page now has content

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,26 +1,58 @@
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  components: {
+    MuiInputLabel: {
+      styleOverrides: {
+        root: {
+          color: "white", // Label text color
+        },
+      },
+    },
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          "&:hover .MuiOutlinedInput-notchedOutline": {
+            borderColor: "white", // Outline color
+          },
+          "& .MuiOutlinedInput-notchedOutline": {
+            borderColor: "white", // Outline color
+          },
+        },
+      },
+    },
+  },
+});
 
 function Login() {
   return (
-    <Box
-      component="form"
-      sx={{
-        "& .MuiTextField-root": { m: 1, width: "25ch" },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <div>
-        <TextField required id="outlined-required" label="Username" />
-        <TextField
-          id="outlined-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-        />
-      </div>
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box
+        component="form"
+        sx={{
+          "& .MuiTextField-root": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <div>
+          <TextField
+            required
+            id="outlined-required"
+            label="Username"
+            InputProps={{ style: { color: "white" } }}
+          />
+          <TextField
+            id="outlined-password-input"
+            label="Password"
+            type="password"
+            InputProps={{ style: { color: "white" } }}
+          />
+        </div>
+      </Box>
+    </ThemeProvider>
   );
 }
 
