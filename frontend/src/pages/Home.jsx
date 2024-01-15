ba6d86aa70df91ae097b75e4442f4b63b18b9527
Patrick Palmroos <palmroos.patrick@gmail.com>
2024-01-15 05:54:20 +0200

    change: vite.config reverted to same as its been but home.jsx has code for displaying if user is logged in. Currently for old system and needs to be redone

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,27 @@
+import { CookiesProvider, useCookies } from "react-cookie";
+
 function Home() {
+  const [cookies] = useCookies(["user"]);
+
   return (
-    <div>
-      <h1>Learn a Language!</h1>
-      <h3>Languages to learn:</h3>
-      <ul>
-        <li>english</li>
-        <li>finnish</li>
-        <li>swedish</li>
-      </ul>
-    </div>
+    <CookiesProvider>
+      <div>
+        <h1>Learn a Language!</h1>
+        <h3>Languages to learn:</h3>
+        <ul>
+          <li>english</li>
+          <li>finnish</li>
+          <li>swedish</li>
+        </ul>
+      </div>
+      <div>
+        {cookies.user ? (
+          <h1>Welcome: {cookies.user.username}</h1>
+        ) : (
+          <h1>Not logged in</h1>
+        )}
+      </div>
+    </CookiesProvider>
   );
 }
 
