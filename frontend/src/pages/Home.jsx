48cd9ba5757bb4e75da064c4b51f93e39e0dbfaf
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-01-16 22:10:50 +0200

    change: home page now displays if user is logged in

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,40 @@
 import { CookiesProvider, useCookies } from "react-cookie";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Home() {
-  const [cookies] = useCookies(["user"]);
+  const [logged, setLogged] = useState(false);
+
+  useEffect(() => {
+    const autoLogin = async () => {
+      try {
+        console.log("called");
+        const resp = await axios.get(
+          `${import.meta.env.VITE_API_URL}/autoLogin`,
+          {
+            withCredentials: true,
+          }
+        );
+        console.log("ok");
+        if (resp.status === 200) {
+          console.log("success");
+          setLogged(true);
+        }
+      } catch (err) {
+        console.log(err);
+        // Handle error
+        if (err.response && err.response.status === 401) {
+          console.log("Unauthorized access");
+        } else {
+          console.error("Unexpected error:", err);
+        }
+      }
+    };
+    autoLogin();
+  }, []);
 
   return (
-    <CookiesProvider>
+    <>
       <div>
         <h1>Learn a Language!</h1>
         <h3>Languages to learn:</h3>
@@ -14,14 +44,8 @@ function Home() {
           <li>swedish</li>
         </ul>
       </div>
-      <div>
-        {cookies.user ? (
-          <h1>Welcome: {cookies.user.username}</h1>
-        ) : (
-          <h1>Not logged in</h1>
-        )}
-      </div>
-    </CookiesProvider>
+      <div>{logged ? <h1>Welcome!</h1> : <h1>Not logged in</h1>}</div>
+    </>
   );
 }
 
