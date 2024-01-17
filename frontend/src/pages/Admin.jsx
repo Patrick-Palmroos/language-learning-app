f605e35b9cefe06bbc0842214dfefcd4094cab31
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-01-17 16:03:33 +0200

    change: now displays test tasks with respective buttons

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,5 +1,22 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Task from "./Task";
+
 function Admin() {
-  return <h1>Admin</h1>;
+  const [logged, setLogged] = useState(false);
+  const [adminLogged, setAdminLogged] = useState(false);
+
+  return (
+    <>
+      <h2>add task:</h2>
+      <p>bla bla blaaaaaa</p>
+      <h2>all tasks:</h2>
+      <ul>
+        <Task task={{ id: 1, english: "poo", finnish: "kakka" }} />
+        <Task task={{ id: 2, english: "test", finnish: "testi" }} />
+      </ul>
+    </>
+  );
 }
 
 export default Admin;
