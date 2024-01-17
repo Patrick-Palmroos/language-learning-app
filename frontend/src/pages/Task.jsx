8d2fb088466de51dd54d2f4f65f9c113489041ac
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-01-17 16:02:31 +0200

    Add: task element which has translations and edit and delete buttons

---
+++ frontend/src/pages/Task.jsx
@@ -0,0 +1,52 @@
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import axios from "axios";
+
+function Task(data) {
+  const task = {
+    id: data.task.id,
+    english: data.task.english,
+    finnish: data.task.finnish,
+  };
+
+  const handleDeletion = async () => {
+    try {
+      const resp = axios.post(
+        `${import.meta.env.VITE_API_URL}/deleteTask`,
+        {
+          id: task.id,
+        },
+        { withCredentials: true }
+      );
+      console.log(resp);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <>
+      <li>
+        <h2>
+          task:{task.id} en:{task.english} fi:{task.finnish}
+        </h2>
+      </li>
+      <Button
+        variant="contained"
+        // endIcon={<LoginOutlinedIcon />}
+        onClick={() => console.log("edit")}
+      >
+        Edit
+      </Button>
+      <Button
+        variant="contained"
+        // endIcon={<LoginOutlinedIcon />}
+        onClick={handleDeletion}
+      >
+        Delete
+      </Button>
+    </>
+  );
+}
+
+export default Task;
