0167f4af6a7bccecd61c14aa76afb1736b3fc394
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-01-17 19:40:50 +0200

    change: can add new task from UI

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,12 +1,45 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Task from "./Task";
+import TextField from "@mui/material/TextField";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+
+const theme = createTheme({
+  components: {
+    MuiInputLabel: {
+      styleOverrides: {
+        root: {
+          color: "white",
+        },
+      },
+    },
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          "&:hover .MuiOutlinedInput-notchedOutline": {
+            borderColor: "white",
+          },
+          "& .MuiOutlinedInput-notchedOutline": {
+            borderColor: "white",
+          },
+        },
+      },
+    },
+  },
+});
 
 function Admin() {
   const [logged, setLogged] = useState(false);
   const [adminLogged, setAdminLogged] = useState(false);
   const [tasks, setTasks] = useState([]);
+  const [finnish, setFinnish] = useState("");
+  const [english, setEnglish] = useState("");
+  const [err, setErr] = useState(null);
+  const navigate = useNavigate();
 
+  //gets all tasks and puts them inside tasks
   useEffect(() => {
     axios
       .get(`${import.meta.env.VITE_API_URL}/tasks`)
@@ -14,18 +47,79 @@ function Admin() {
       .catch((err) => console.log(err));
   }, []);
 
+  //handles pressing the new task button
+  const handleNewTask = async () => {
+    if (finnish.trim() !== "" && english.trim() !== "") {
+      if (err !== null) {
+        setErr(null);
+      }
+      //sends a post containing login credentials to check for backend
+      try {
+        const resp = await axios.post(
+          `${import.meta.env.VITE_API_URL}/addTask`,
+          {
+            english: english,
+            finnish: finnish,
+          },
+          { withCredentials: true }
+        );
+        //if response is 200, refreshes page
+        if (resp.status === 200) {
+          navigate(0);
+        }
+      } catch (err) {
+        //error sets err to be "invalid input"
+        setErr("invalid input");
+      }
+    } else {
+      //if fields are empty sets error message
+      setErr("both fields need to be filled");
+    }
+  };
+
   return (
     <>
-      <h2>add task:</h2>
-      <p>bla bla blaaaaaa</p>
+      <h2>New task:</h2>
+      <div>
+        <ThemeProvider theme={theme}>
+          <TextField
+            required
+            id="english"
+            label="english"
+            InputProps={{ style: { color: "white" } }}
+            onChange={(input) => setEnglish(input.target.value)}
+            error={err === null ? false : true}
+            helperText={err}
+          />
+          <TextField
+            required
+            id="finnish"
+            label="finnish"
+            InputProps={{ style: { color: "white" } }}
+            onChange={(input) => setFinnish(input.target.value)}
+            error={err === null ? false : true}
+            helperText={err}
+          />
+        </ThemeProvider>
+      </div>
+      <Button
+        variant="contained"
+        // endIcon={<LoginOutlinedIcon />}
+        onClick={handleNewTask}
+      >
+        add task
+      </Button>
       <h2>all tasks:</h2>
       <ul>
         {tasks.map((task) => {
-          console.log(task);
           return (
             <Task
-              key={task.id}
-              task={{ id: 1, english: task.English, finnish: task.Finnish }}
+              key={task.TaskID}
+              task={{
+                id: task.TaskID,
+                english: task.English,
+                finnish: task.Finnish,
+              }}
             />
           );
         })}
