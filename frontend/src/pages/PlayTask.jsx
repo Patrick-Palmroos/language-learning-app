ed367828e5f37687e5520d1f1f90db357ce5f5cf
Patrick-Palmroos <palmroos.patrick@gmail.com>
2024-01-18 04:51:16 +0200

    change: slight rework. No longer updates score amount. instead communicates with parent of relevant info

--- frontend/src/pages/PlayTask.jsx
+++ frontend/src/pages/PlayTask.jsx
@@ -1,8 +1,6 @@
-import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { useState } from "react";
-import axios from "axios";
+import { useEffect, useState } from "react";
 
 //theme for default mui boxes
 const theme = createTheme({
@@ -54,36 +52,43 @@ const success = createTheme({
   },
 });
 
-function PlayTask(task) {
+function PlayTask(prop) {
   const [color, setColor] = useState(theme);
   const [answer, setAnswer] = useState("");
   const [err, setErr] = useState(null);
   const [answered, setAnswered] = useState(false);
 
   //checks if answer is correct.
-  const checkAnswer = async () => {
-    setAnswered(true);
-    if (answer === task.data.Finnish) {
-      //if answer was correct selects different color theme and increments users score.
-      setColor(success);
-      const resp = await axios.get(`${import.meta.env.VITE_API_URL}/newScore`, {
-        withCredentials: true,
-      });
-      if (resp.status !== 200) {
-        console.log("Error with sending score to backend");
+  if (prop.answered && !answered) {
+    const checkAnswer = async () => {
+      setAnswered(true);
+      if (answer === prop.prop.Finnish) {
+        //if answer was correct selects different color theme and increments users score.
+        setColor(success);
+      } else {
+        //if answer was wrong, sents text field to correct answer and sets an error
+        setErr("Wrong! Correct answer was:");
+        setAnswer(prop.prop.Finnish.toString());
       }
+    };
+
+    checkAnswer();
+  }
+
+  //if answer changes, uses callback to send relevant info back to parent
+  useEffect(() => {
+    if (answer === prop.prop.Finnish) {
+      prop.callback(prop.prop.TaskID, true);
     } else {
-      //if answer was wrong, sents text field to correct answer and sets an error
-      setErr("Wrong! Correct answer was:");
-      setAnswer(task.data.Finnish.toString());
+      prop.callback(prop.prop.TaskID, false);
     }
-  };
+  }, [answer]);
 
   return (
     <>
       <div className="task">
         <ThemeProvider theme={color}>
-          <h2>{task.data.English}: </h2>
+          <h2>{prop.prop.English}: </h2>
           <TextField
             required
             id="finnish"
@@ -95,18 +100,7 @@ function PlayTask(task) {
             helperText={err}
           />
         </ThemeProvider>
-        {answered && color === success ? (
-          <p>+1p</p>
-        ) : (
-          <Button
-            variant="contained"
-            size="small"
-            onClick={checkAnswer}
-            disabled={answered}
-          >
-            check
-          </Button>
-        )}
+        {answered && color === success ? <p>+1p</p> : null}
       </div>
     </>
   );
